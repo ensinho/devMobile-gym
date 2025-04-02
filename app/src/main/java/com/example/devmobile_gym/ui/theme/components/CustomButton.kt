70b5a261c17daa3637700e94279df60ec5623c87
Kaio <kakacnmendes@gmail.com>
2025-04-02 09:10:57 -0300

    Style: altera o tamanho do Componente CustomButton e de seu texto interno

--- app/src/main/java/com/example/devmobile_gym/ui/theme/components/CustomButton.kt
+++ app/src/main/java/com/example/devmobile_gym/ui/theme/components/CustomButton.kt
@@ -1,6 +1,7 @@
 package com.example.components.ui.theme.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -9,6 +10,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun CustomButton(
@@ -20,12 +22,13 @@ fun CustomButton(
         onClick = onClick,
         colors = ButtonDefaults.buttonColors(containerColor = backgroundColor),
         shape = RoundedCornerShape(8.dp), // Bordas arredondadas
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth().height(34.dp)
     ) {
         Text(
             text = text,
             color = Color.White,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp
         )
     }
 }

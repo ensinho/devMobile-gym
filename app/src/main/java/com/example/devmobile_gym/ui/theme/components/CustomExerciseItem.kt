e884457e1f1ca4fa8e4dcad942300a9564d4122d
Kaio <kakacnmendes@gmail.com>
2025-04-11 11:34:23 -0300

    Style: Adiciona card para adicionar e remover exercicios de uma rotina

---
+++ app/src/main/java/com/example/devmobile_gym/ui/theme/components/CustomExerciseItem.kt
@@ -0,0 +1,100 @@
+package com.example.devmobile_gym.ui.theme.components
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.devmobile_gym.R
+
+/*No momento, não temos uma classe exercício, então trabalhamos usando dois parâmetros diferentes.*/
+@Composable
+fun CustomExerciseItem(
+    exercise: String,
+    description: String
+) {
+    var isIncluded by remember { mutableStateOf(false) }
+    Card (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(2.dp)
+            .animateContentSize(),
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFF2E3238))
+    ) {
+        Row (
+            modifier = Modifier.padding(12.dp).fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Column (
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Center
+            ){
+                Text(
+                    text = exercise,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+                Spacer(Modifier.height(6.dp))
+                Text(
+                    text = "• $description",
+                    fontSize = 13.sp,
+                    color = Color(0xFFAAAEB6),
+                    modifier = Modifier.padding(start = 10.dp)
+                )
+
+            }
+
+            IconButton(
+                onClick = {isIncluded = !isIncluded},
+            ) {
+                Icon(
+                    painter = painterResource(id = if (isIncluded) R.drawable.remove_item else R.drawable.add_circle_item),
+                    contentDescription = "Adicionar",
+                    tint = if (isIncluded) Color.Red else Color.Green,
+                    modifier = Modifier.size(35.dp)
+                )
+            }
+
+
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewCustomExerciseItem() {
+    CustomExerciseItem("Agachamento Livre", "Quadríceps e posterior")
+}

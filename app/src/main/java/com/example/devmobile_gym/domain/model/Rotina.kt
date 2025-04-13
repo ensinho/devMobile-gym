6722a529f7ef5eb02240ce6c6fd62c77f5c6c61d
Kaio <kakacnmendes@gmail.com>
2025-04-13 10:48:31 -0300

    feat: Adiciona os models das classes do sistema

--- app/src/main/java/com/example/devmobile_gym/domain/model/Rotina.kt
+++ app/src/main/java/com/example/devmobile_gym/domain/model/Rotina.kt
@@ -3,7 +3,6 @@ package com.example.devmobile_gym.domain.model
 data class Rotina(
     val id: Int,
     val nome: String,
-    val descricao: String,
     val treinos: List<Treino>
 
 )
--- app/src/main/java/com/example/devmobile_gym/domain/model/Treino.kt
+++ app/src/main/java/com/example/devmobile_gym/domain/model/Treino.kt
@@ -3,7 +3,6 @@ package com.example.devmobile_gym.domain.model
 data class Treino(
     val id: Int,
     val nome: String,
-    val descricao: String,
     val exercicios: List<Exercicio>
 
 )

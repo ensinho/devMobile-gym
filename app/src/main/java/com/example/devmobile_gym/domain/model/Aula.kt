6cc54c76fea6940d85c6ab15de2b9ba3debbd032
Kaio <kakacnmendes@gmail.com>
2025-04-17 16:45:24 -0300

    feat: Adiciona o model aula

--- app/src/main/java/com/example/devmobile_gym/domain/model/Aula.kt
+++ app/src/main/java/com/example/devmobile_gym/domain/model/Aula.kt
@@ -1,4 +1,8 @@
 package com.example.devmobile_gym.domain.model
 
-class Aula {
-}
+data class Aula(
+    val id: Int,
+    val professor: Professor,
+    val alocacaoMaxima: Int,
+    val data: String // ou LocalDate, se preferir
+)

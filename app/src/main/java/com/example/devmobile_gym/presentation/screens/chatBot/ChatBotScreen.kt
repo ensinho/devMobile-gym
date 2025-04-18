f6094a151532971f10456147138ff12709b1b08f
Kaio <kakacnmendes@gmail.com>
2025-04-18 14:02:06 -0300

    feat: adiciona a tela do chatbot

--- app/src/main/java/com/example/devmobile_gym/presentation/screens/chatBot/ChatBotScreen.kt
+++ app/src/main/java/com/example/devmobile_gym/presentation/screens/chatBot/ChatBotScreen.kt
@@ -1,2 +1,81 @@
 package com.example.devmobile_gym.presentation.screens.chatBot
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.components.ui.theme.components.CustomButton
+import com.example.devmobile_gym.presentation.components.CustomScreenScaffold
+import com.example.devmobile_gym.presentation.components.CustomTextField
+
+@Composable
+fun ChatBotScreen(viewModel: ChatBotViewModel = viewModel(), onBack: () -> Unit) {
+//    lista de mensagens do chat
+    val messages = viewModel.message
+//    input do usuario
+    val userInput = viewModel.userInputs
+
+    CustomScreenScaffold(
+        title = "ChatBot",
+        onBackClick = { onBack() },
+        onMenuClick = { /* Handle menu click */ },
+        needToGoBack = true,
+        content = { innerModifier ->
+            val combinedModifier = innerModifier.padding(0.5.dp)
+
+            Column (
+                modifier = combinedModifier.fillMaxSize()
+            ){
+                LazyColumn (
+                    modifier = Modifier.weight(1.5f)
+                ){
+                    items(messages) { message ->
+                        Text(
+                            text = message.text,
+                            fontSize = 16.sp,
+                            modifier = Modifier.align(if (message.isUser) Alignment.End else Alignment.Start)
+                        )
+                    }
+                }
+
+                Row (
+                    modifier = Modifier.weight(0.17f),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ){
+                    Column {
+
+                        CustomTextField(
+                            value = userInput.value,
+                            onValueChange = { viewModel.onUserInputChange(it) },
+                            label = "Digite sua mensagem",
+                            padding = 0,
+                            modifier = Modifier.width(270.dp)
+                        )
+                    }
+
+                    Column {
+                        CustomButton(
+                            text = "Enviar",
+                            onClick = {
+                                viewModel.sendMessage(userInput.value)
+                            },
+                            modifier = Modifier
+                        )
+                    }
+
+                }
+            }
+        }
+    )
+}
--- app/src/main/java/com/example/devmobile_gym/presentation/screens/chatBot/ChatBotViewModel.kt
+++ app/src/main/java/com/example/devmobile_gym/presentation/screens/chatBot/ChatBotViewModel.kt
@@ -1,4 +1,51 @@
 package com.example.devmobile_gym.presentation.screens.chatBot
 
-class ChatBotViewModel {
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.devmobile_gym.domain.model.Message
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class ChatBotViewModel : ViewModel() {
+    private val _message = mutableStateListOf<Message>()
+    val message: List<Message> = _message
+
+    private val _userInputs = mutableStateOf("")
+    val userInputs: State<String> = _userInputs
+
+    fun onUserInputChange(newInput: String) {
+        _userInputs.value = newInput
+
+    }
+
+    fun sendMessage(userMessage: String) {
+        if (userMessage.isNotEmpty()) {
+            _message.add(Message(text = userMessage, isUser = true))
+            _userInputs.value = ""
+
+            // Resposta do chatbot
+            simulateChatBotResponse(userMessage)
+        }
+
+    }
+
+    private fun generateBotResponse(userMessage: String): String {
+        return when {
+            userMessage.contains("olá", ignoreCase = true) -> "Olá! Como posso ajudar?"
+            userMessage.contains("treino", ignoreCase = true) -> "Posso te explicar sobre exercícios e máquinas!"
+            else -> "Desculpe, ainda estou aprendendo. Tente perguntar de outra forma!!"
+
+        }
+    }
+
+    private fun simulateChatBotResponse(userMessage: String) {
+        viewModelScope.launch {
+            delay(1000)
+            val botResponse = generateBotResponse(userMessage)
+            _message.add(Message(text = botResponse, isUser = false))
+        }
+    }
 }

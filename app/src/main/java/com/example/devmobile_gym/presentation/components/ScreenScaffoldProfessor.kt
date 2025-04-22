172894e2bf52f3ad17e801219622a6f1f6865173
lucas <lucasbarrososainfinity@gmail.com>
2025-04-22 03:01:09 -0300

    adicionando ProfessorHomeScreen.ktm, ScreenScaffoldProfessor.kt e icones

---
+++ app/src/main/java/com/example/devmobile_gym/presentation/components/ScreenScaffoldProfessor.kt
@@ -0,0 +1,260 @@
+package com.example.devmobile_gym.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.devmobile_gym.R
+
+// classe que representa cada item da navbar
+data class BottomNavigationItemProfessor(
+    val title: String,
+    // o NavIcon se adapta de acordo com o valor passado para os icons (VectorIcon ou DrawableIcon)
+    val selectedIcon: NavIcon,
+    val unselectedIcon: NavIcon,
+    val hasNews: Boolean,
+    val badgeCount: Int? = null
+)
+
+// sealed class que suporta as variações de tipo do selectedIcon e do unselectedIcon
+/*LEMBRETE o ":" significa herança.
+* VectorIcon e DrawableIcon herdam NavIcon.
+*/
+sealed class NavIconProfessor {
+    data class VectorIcon(val icon: ImageVector) : NavIcon()
+    data class DrawableIcon(val resId: Int) : NavIcon()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomScreenScaffoldProfessor(
+    navController: NavHostController,
+    title: String,
+    needToGoBack: Boolean = false,
+    onBackClick: () -> Unit,
+    onMenuClick: () -> Unit,
+    selectedItemIndex: Int,
+    content: @Composable (Modifier) -> Unit
+) {
+//    forma que a topbar funciona (nesse caso ela some se scrollar pra baixo e volta se scrollar um pouco pra cima)
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    // mocka os itens da navbar do usuario
+    // futuramente vamos precisar validar o usuario (se é normal ou professor) para escolher entre esses itens e os itens do professor
+    val itemsUser = listOf(
+        /* HOME */
+        BottomNavigationItemProfessor(
+            title = "Home",
+            selectedIcon = NavIconProfessor.DrawableIcon(R.drawable.home_icon),
+            unselectedIcon = NavIconProfessor.DrawableIcon(R.drawable.home_icon),
+            hasNews = false
+            /* badgeCount = null (valor padrão)*/
+        ),
+        /* SEARCH */
+        BottomNavigationItemProfessor(
+            title = "Aulas  ",
+            selectedIcon = NavIconProfessor.DrawableIcon(R.drawable.ic_treinos),
+            unselectedIcon = NavIconProfessor.DrawableIcon(R.drawable.ic_treinos),
+            hasNews = false
+            /* badgeCount = null (valor padrão)*/
+        ),
+        /* SCAN QR CODE */
+        BottomNavigationItemProfessor(
+
+            title = "QR Code",
+            selectedIcon = NavIconProfessor.DrawableIcon(R.drawable.ic_centroprofessor),
+            unselectedIcon = NavIconProfessor.DrawableIcon(R.drawable.ic_centroprofessor),
+            hasNews = false
+            /* badgeCount = null (valor padrão)*/
+        ),
+        /* CHAT */
+        BottomNavigationItemProfessor(
+            title = "ChatBot",
+            selectedIcon = NavIconProfessor.DrawableIcon(R.drawable.chat_icon_filled),
+            unselectedIcon = NavIconProfessor.DrawableIcon(R.drawable.chat_icon_outlined),
+            hasNews = false
+            /* badgeCount = null (valor padrão)*/
+        ),
+        /* PROFILE */
+        BottomNavigationItemProfessor(
+            title = "Gerenciar",
+            selectedIcon = NavIconProfessor.DrawableIcon(R.drawable.ic_chave_fenda),
+            unselectedIcon = NavIconProfessor.DrawableIcon(R.drawable.ic_chave_fenda),
+            hasNews = false
+            /* badgeCount = null (valor padrão)*/
+        )
+    )
+
+
+
+
+    Scaffold (
+
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            if (needToGoBack) {
+
+                CenterAlignedTopAppBar(
+                    title = { Text(title) }
+                    ,
+                    navigationIcon = {
+                        IconButton(onClick = { onBackClick() }) {
+                            Icon(Icons.Default.KeyboardArrowLeft, contentDescription = "Voltar para a tela anterior.")
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = { onMenuClick() }) {
+                            Icon(Icons.Default.Menu, contentDescription = "Abrir menu lateral.")
+                        }
+                    },
+                    scrollBehavior = scrollBehavior
+
+                )
+            } else {
+                TopAppBar(
+                    title = { Text(title) }
+                    ,
+                    actions = {
+                        IconButton(onClick = { onMenuClick() }) {
+                            Icon(Icons.Default.Menu, contentDescription = "Abrir menu lateral.")
+                        }
+                    },
+                    scrollBehavior = scrollBehavior
+
+                )
+            }
+        },
+        bottomBar = {
+            NavigationBar(
+                containerColor = Color(0xFF2B2B2B),
+                tonalElevation = 6.dp,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(bottomStart = 32.dp, bottomEnd = 32.dp))
+            ) {
+                itemsUser.forEachIndexed { index, item ->
+                    if (index == 2) {
+                        // Botão central customizado
+                        IconButton(
+                            onClick = {
+                                navController.navigate("qrcode")
+                            },
+                            modifier = Modifier
+                                .padding(top = 0.dp)
+                                .background(Color.White, shape = CircleShape)
+                                .padding(3.dp) // Borda branca
+                                .background(Color(0xFF1E88E5), shape = CircleShape) // Fundo azul
+                                .padding(6.dp) // Espaçamento interno
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_centroprofessor),
+                                contentDescription = item.title,
+                                tint = Color.White,
+                                modifier = Modifier
+                                    .height(24.dp)
+                            )
+                        }
+                    } else {
+                        NavigationBarItem(
+                            selected = selectedItemIndex == index,
+                            onClick = {
+                                when(index) {
+                                    0 -> navController.navigate("home")
+                                    1 -> navController.navigate("search")
+                                    3 -> navController.navigate("chatbot")
+                                    4 -> navController.navigate("profile")
+                                }
+                            },
+                            label = {
+                                Text(item.title,color = Color.White)
+                            },
+                            alwaysShowLabel = true,
+                            icon = {
+                                BadgedBox(
+                                    badge = {
+                                        when {
+                                            item.badgeCount != null -> {
+                                                Badge {
+                                                    Text(text = item.badgeCount.toString())
+                                                }
+                                            }
+                                            item.hasNews -> {
+                                                Badge()
+                                            }
+                                        }
+                                    }
+                                ) {
+                                    val iconToShow = if (index == selectedItemIndex) item.selectedIcon else item.unselectedIcon
+
+                                    when (iconToShow) {
+                                        is NavIconProfessor.VectorIcon -> {
+                                            Icon(
+                                                imageVector = iconToShow.icon,
+                                                contentDescription = item.title
+                                            )
+                                        }
+
+                                        is NavIconProfessor.DrawableIcon -> {
+                                            Icon(
+                                                painter = painterResource(id = iconToShow.resId),
+                                                contentDescription = item.title
+                                            )
+                                        }
+                                        else -> {}
+                                    }
+                                }
+                            }
+                        )
+                    }
+                }
+            }
+        }
+
+
+    ) { innerPadding ->
+        content(Modifier.padding(innerPadding))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ScreenScaffoldPreview() {
+//    CustomScreenScaffold(
+//        title = "Título",
+//        needToGoBack = true,
+//        onBackClick = {},
+//        onMenuClick = {},
+//        content = { modifier -> Text("Conteúdo aqui", modifier = modifier.padding(16.dp)) }
+//    )
+}
--- app/src/main/java/com/example/devmobile_gym/presentation/screens/home/ProfessorHomeScreen.kt
+++ app/src/main/java/com/example/devmobile_gym/presentation/screens/home/ProfessorHomeScreen.kt
@@ -1,2 +1,119 @@
 package com.example.devmobile_gym.presentation.screens.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.components.ui.theme.components.CustomButton
+import com.example.devmobile_gym.presentation.components.CustomCalendario
+import com.example.devmobile_gym.presentation.components.CustomCard
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.devmobile_gym.R
+import com.example.devmobile_gym.domain.model.Aluno
+import com.example.devmobile_gym.presentation.components.CardHomeProfessor
+import com.example.devmobile_gym.presentation.components.CustomScreenScaffoldProfessor
+import com.example.devmobile_gym.ui.theme.White
+
+@Composable
+fun ProfessorHomeScreen(navController: NavHostController, viewModel: ProfessorHomeViewModel = viewModel()) {
+    val alunos by viewModel.alunos
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    val selectedItemIndex = when (currentRoute) {
+        "home" -> 0
+        "search" -> 1
+        "qrcode" -> 2
+        "chatbot" -> 3
+        "profile" -> 4
+        else -> 0 // default
+    }
+
+    CustomScreenScaffoldProfessor  (
+        navController = navController,
+        title = "Home",
+        onBackClick = { /* Handle back click */ },
+        onMenuClick = { /* Handle menu click */ },
+        selectedItemIndex = selectedItemIndex,
+        content = { innerModifier ->
+            val combinedModifier = innerModifier.padding(1.dp)
+
+            LazyColumn(
+                modifier = combinedModifier
+            ) {
+                items(alunos) { alunos ->
+                    CardHomeProfessor(
+                        texto = alunos.nome,
+                        icone = R.drawable.ic_caneta
+                    )
+                    Spacer(Modifier.height(8.dp))
+
+                }
+
+            }
+        }
+    )
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfessorHomePreview() {
+    val fakeNavController = rememberNavController()
+    val fakeAlunos = listOf(
+        Aluno(1,"renan","","",),
+        Aluno(2,"joao","","",),
+        Aluno(3,"Lucas","","",)
+    )
+
+    // Recria o conteúdo da tela usando dados mockados
+    CustomScreenScaffoldProfessor(
+        navController = fakeNavController,
+        title = "Home",
+        onBackClick = {},
+        onMenuClick = {},
+        selectedItemIndex = 0,
+        content = { innerModifier ->
+            val combinedModifier = innerModifier.padding(0.dp)
+                .background(Color(0xFF1E1E1E))
+            LazyColumn(modifier = combinedModifier
+                .fillMaxSize()) {
+
+                item {
+                    Text(modifier = Modifier
+                        .padding(10.dp),
+                        color = White,
+                        text = "Painel de Alunos",
+                        fontSize = 35.sp
+                    )
+                }
+                items(fakeAlunos) { aluno ->
+                    CardHomeProfessor(
+                        texto = aluno.nome,
+                        icone = R.drawable.ic_caneta
+                    )
+                    Spacer(Modifier.height(4.dp))
+                }
+            }
+        }
+    )
+}

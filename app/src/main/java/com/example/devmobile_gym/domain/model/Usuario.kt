5d7a8831c68c5c948797ebfb10c31929b49934f8
Kaio Mendes <94843909+kaiomendes15@users.noreply.github.com>
2025-05-13 20:23:36 -0300

    Update Usuario.kt

--- app/src/main/java/com/example/devmobile_gym/domain/model/Usuario.kt
+++ app/src/main/java/com/example/devmobile_gym/domain/model/Usuario.kt
@@ -5,3 +5,11 @@ data class Usuario(
     val nome: String,
     var isProfessor: Boolean = false
 )
+
+/* 
+    sealed class Usuario(val email: String, val nome: String) {
+    data class Professor(val email: String, val nome: String, val area: String) : Usuario(email, nome)
+    data class Aluno(val email: String, val nome: String, val matricula: String) : Usuario(email, nome)
+    }
+
+*/

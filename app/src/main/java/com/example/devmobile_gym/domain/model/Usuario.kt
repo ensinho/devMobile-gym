dabda0c9b713486eae6b4a8993aef88e836f4225
Kaio <kakacnmendes@gmail.com>
2025-05-17 13:40:28 -0300

    refactor: corrige a classe usuario para ter um construtor vazio, necessario para o firestore, e altera o tipo da lista rotina

--- app/src/main/java/com/example/devmobile_gym/domain/model/Usuario.kt
+++ app/src/main/java/com/example/devmobile_gym/domain/model/Usuario.kt
@@ -1,21 +1,24 @@
 package com.example.devmobile_gym.domain.model
 
+import com.google.firebase.firestore.IgnoreExtraProperties
+
 // Arquivo: domain/model/User.kt
 sealed class Usuario(
     open val uid: String?,
     open val nome: String,
     open val email: String?
 ) {
+    @IgnoreExtraProperties
     data class Aluno(
-        override val uid: String?,
-        override val nome: String,
-        override val email: String?,
-        val rotina: List<String>? = null
+        override val uid: String? = "",
+        override val nome: String = "",
+        override val email: String? = "",
+        val rotina: List<Rotina>? = emptyList()
     ) : Usuario(uid, nome, email)
 
     data class Professor(
-        override val uid: String?,
-        override val nome: String,
-        override val email: String?
+        override val uid: String? = "",
+        override val nome: String = "",
+        override val email: String? = ""
     ) : Usuario(uid, nome, email)
 }

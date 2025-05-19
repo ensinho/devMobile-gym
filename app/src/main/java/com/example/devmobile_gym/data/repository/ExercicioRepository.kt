425ed987646a34ba5af3541c79370b71bf66a377
Kaio <kakacnmendes@gmail.com>
2025-05-19 10:13:10 -0300

    feat: adiciona os metodos para visualizar todos os exercicios e para inserir um novo exercicio no banco

--- app/src/main/java/com/example/devmobile_gym/data/repository/ExercicioRepository.kt
+++ app/src/main/java/com/example/devmobile_gym/data/repository/ExercicioRepository.kt
@@ -1,41 +1,51 @@
 package com.example.devmobile_gym.data.repository
 
+import android.util.Log
 import com.example.devmobile_gym.data.mock.MockData
 import com.example.devmobile_gym.domain.model.Exercicio
 import com.example.devmobile_gym.domain.repository.ExercicioRepositoryModel
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.tasks.await
 
 class ExercicioRepository : ExercicioRepositoryModel {
-
+    private val db = FirebaseFirestore.getInstance()
+    private val exerciciosCollection = db.collection("exercicios")
 //    override fun getExercicio(exercicioId: Int): Exercicio? {
 //        return MockData.rotinaMock.treinos
 //            .flatMap { it.exercicios }
 //            .find { it.id == exercicioId }
 //    }
 
-    override fun getAllExercicios(): List<Exercicio> {
-        return MockData.rotinaMock.treinos
-            .flatMap { it.exercicios }
+    override suspend fun getAllExercicios(): List<Exercicio> {
+        val exercicios = MutableStateFlow<List<Exercicio>>(emptyList())
+        db.collection("exercicios")
+            .get()
+            .addOnSuccessListener { result ->
+                val listaExercicios = result.map { document ->
+                    Exercicio(
+                        uid = document.id,
+                        nome = document.getString("nome") ?: "",
+                        grupoMuscular = document.getString("grupoMuscular") ?: "",
+                        imagem = document.getString("imagem") ?: ""
+                    )
+                }
+                exercicios.value = listaExercicios
+            }
+            .addOnFailureListener {
+                Log.e("ExercicioRepository", "Erro ao buscar exercícios", it)
+            }
+            .await()
+
+        return exercicios.value
     }
 
-//    override fun editarExercicio(
-//        exercicioId: Int,
-//        nome: String,
-//        series: Int,
-//        grupoMuscular: String,
-//        repeticoes: Int,
-//        peso: Int
-//    ): Boolean {
-//        MockData.rotinaMock.treinos.forEach { treino ->
-//            treino.exercicios.forEach { exercicio ->
-//                if (exercicio.id == exercicioId) {
-//                    exercicio.nome = nome
-//                    exercicio.series = series
-//                    exercicio.repeticoes = repeticoes
-//                    exercicio.peso = peso
-//                    return true
-//                }
-//            }
-//        }
-//        return false
-//    }
+    override suspend fun insertExercicio(exercicio: Exercicio) {
+        try {
+            exerciciosCollection.add(exercicio).await()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw e
+        }
+    }
 }

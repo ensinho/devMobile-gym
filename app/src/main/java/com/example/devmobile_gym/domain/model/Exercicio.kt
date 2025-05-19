a5d35e425592b2dddeb59540ffe30d41af8c651d
Kaio <kakacnmendes@gmail.com>
2025-05-19 10:13:57 -0300

    refactor: altera a classe para que ela possua um construtor vazio e possa receber um uid

--- app/src/main/java/com/example/devmobile_gym/domain/model/Exercicio.kt
+++ app/src/main/java/com/example/devmobile_gym/domain/model/Exercicio.kt
@@ -1,6 +1,7 @@
 package com.example.devmobile_gym.domain.model
 
 data class Exercicio(
+    val uid: String = "",
     val nome: String = "",
     val grupoMuscular: String = "",
     val imagem: String = ""

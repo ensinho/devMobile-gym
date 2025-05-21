ce4020ba6b580648d19bdd33ecc46652179f9f19
Kaio <kakacnmendes@gmail.com>
2025-05-21 14:47:53 -0300

    feat: adiciona metodo para receber todas as aulas

--- app/src/main/java/com/example/devmobile_gym/data/repository/AulaRepository.kt
+++ app/src/main/java/com/example/devmobile_gym/data/repository/AulaRepository.kt
@@ -1,12 +1,28 @@
 package com.example.devmobile_gym.data.repository
 
-import com.example.devmobile_gym.data.mock.MockData
+import android.util.Log
 import com.example.devmobile_gym.domain.model.Aula
+import com.example.devmobile_gym.domain.model.Exercicio
 import com.example.devmobile_gym.domain.repository.AulaRepositoryModel
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.tasks.await
+import com.google.firebase.firestore.ktx.toObjects
+import java.util.Date
 
 class AulaRepository : AulaRepositoryModel {
-    override fun getAula(): List<Aula?> {
-        TODO("Not yet implemented")
+    private val db = FirebaseFirestore.getInstance()
+    private val aulasCollection = db.collection("aulas")
+
+    override suspend fun getAulas(): List<Aula> {
+        return try {
+            val snapshot = aulasCollection.get().await()
+            snapshot.toObjects(Aula::class.java) // retorna as aulas como uma lista de objetos Aula
+        } catch (e: Exception) {
+            Log.e("AulaRepository", "Erro ao buscar aulas", e)
+            emptyList() // retorna uma lista vazia em caso de erro
+        }
     }
+
 
 }

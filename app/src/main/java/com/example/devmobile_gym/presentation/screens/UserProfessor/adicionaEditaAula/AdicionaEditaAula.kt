6c74527785a5fb4fe85dbedab1069dee8d67b24b
Kaio <kakacnmendes@gmail.com>
2025-05-21 16:23:35 -0300

    feat: adiciona a funcionalidade de adicionar aulas

--- app/src/main/java/com/example/devmobile_gym/presentation/screens/UserProfessor/adicionaEditaAula/AdicionaEditaAula.kt
+++ app/src/main/java/com/example/devmobile_gym/presentation/screens/UserProfessor/adicionaEditaAula/AdicionaEditaAula.kt
@@ -1,17 +1,22 @@
 package com.example.devmobile_gym.presentation.screens.UserProfessor.adicionaEditaAula
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -21,12 +26,23 @@ import com.example.devmobile_gym.presentation.components.BoxSeta2
 import com.example.devmobile_gym.presentation.components.CustomScreenScaffold
 import com.example.devmobile_gym.presentation.components.CustomScreenScaffoldProfessor
 import com.example.devmobile_gym.presentation.components.CustomTextField
+import com.example.devmobile_gym.presentation.components.DateTimePickerInput
 import com.example.devmobile_gym.presentation.navigation.AlunoRoutes
 import com.example.devmobile_gym.presentation.navigation.ProfessorRoutes
+import com.example.components.ui.theme.components.CustomButton
+import com.example.devmobile_gym.domain.model.Aula
+import com.example.devmobile_gym.ui.theme.White
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 @Composable
-fun AdicionaEditaAula(navController: NavHostController, onBack: () -> Unit, viewmodel: AdicionaEditaAulaViewModel = viewModel()) {
+fun AdicionaEditaAula(
+    navController: NavHostController,
+    onBack: () -> Unit,
+    viewmodel: AdicionaEditaAulaViewModel = viewModel()
+) {
 
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
@@ -39,8 +55,14 @@ fun AdicionaEditaAula(navController: NavHostController, onBack: () -> Unit, view
         else -> 0
     }
 
-    val tipoAula by viewmodel.tipoAula
-    val horario by viewmodel.horario
+    val tipoAula by viewmodel.tipoAula.collectAsState()
+    val dataHoraSelecionada by viewmodel.dataHoraSelecionada.collectAsState()
+    val professor by viewmodel.professor.collectAsState()
+    val alocacaoMax by viewmodel.alocacaoMax.collectAsState()
+    val status by viewmodel.status.collectAsState()
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+
     CustomScreenScaffoldProfessor (
         navController = navController,
         onBackClick = { onBack() },
@@ -58,6 +80,12 @@ fun AdicionaEditaAula(navController: NavHostController, onBack: () -> Unit, view
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
+                    Text(
+                        text = "Criar Aula ou Funcional",
+                        fontSize = 30.sp,
+                        color = White,
+                        modifier = Modifier.padding(start = 10.dp, top = 10.dp)
+                    )
                     CustomTextField(
                         label = "Tipo da aula",
                         value = tipoAula,
@@ -66,22 +94,67 @@ fun AdicionaEditaAula(navController: NavHostController, onBack: () -> Unit, view
                         modifier = Modifier
                     )
                     Spacer(modifier = Modifier.height(6.dp))
-                    BoxSeta2("Data")
-                    Spacer(modifier = Modifier.height(6.dp))
-                    BoxSeta2("Professor")
-                    Spacer(modifier = Modifier.height(6.dp))
-                    BoxSeta2("Alocação max")
+                    DateTimePickerInput(
+                        selectedDateTime = dataHoraSelecionada,
+                        onDateTimeSelected = { novaData ->
+                            viewmodel.atualizarDataHora(novaData)
+                        }
+                    )
                     Spacer(modifier = Modifier.height(6.dp))
                     CustomTextField(
-                        label = "Horário",
-                        value = horario,
-                        onValueChange = viewmodel::onHorarioChange,
+                        label = "Professor",
+                        value = professor,
+                        onValueChange = viewmodel::onProfessorChange,
                         padding = 10,
                         modifier = Modifier
                     )
-                    Button(onClick = { /*TODO*/ }) {
-                        Text(text = "Concluir", fontSize = 12.sp)
-                    }
+                    Spacer(modifier = Modifier.height(6.dp))
+                    CustomTextField(
+                        label = "Alocação Máxima",
+                        value = alocacaoMax,
+                        onValueChange = viewmodel::onAlocacaoMaxChange,
+                        padding = 10,
+                        modifier = Modifier
+                    )
+                    Spacer(modifier = Modifier.height(6.dp))
+
+                    CustomButton(
+                        text = "Concluir",
+                        onClick = {
+                            if (viewmodel.isNumeroInteiro(alocacaoMax)) {
+                                val novaAula = dataHoraSelecionada?.let {
+                                    Aula(
+                                        aula = tipoAula,
+                                        dataHora = it,
+                                        professor = professor,
+                                        quantidade_maxima_alunos = alocacaoMax.toInt()
+                                    )
+                                }
+
+                                viewmodel.criarAula(
+                                    aula = novaAula,
+                                    onSuccess = {
+                                        navController.popBackStack()
+                                        coroutineScope.launch {
+                                            delay(300L) // espera meio segundo (300 milissegundos)
+                                            Toast.makeText(context, status, Toast.LENGTH_SHORT).show()
+                                        }
+                                    },
+                                    onError = {
+                                        coroutineScope.launch {
+                                            delay(300L) // espera meio segundo (300 milissegundos)
+                                            Toast.makeText(context, status, Toast.LENGTH_SHORT).show()
+                                        }
+                                    }
+                                )
+                            } else {
+                                Toast.makeText(context, "Alocação Máxima deve ser um número inteiro.", Toast.LENGTH_SHORT).show()
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp) // opcional: deixa o botão com tamanho fixo
+                    )
                 }
             }
         }
--- app/src/main/java/com/example/devmobile_gym/presentation/screens/UserProfessor/adicionaEditaAula/AdicionaEditaAulaViewModel.kt
+++ app/src/main/java/com/example/devmobile_gym/presentation/screens/UserProfessor/adicionaEditaAula/AdicionaEditaAulaViewModel.kt
@@ -3,20 +3,72 @@ package com.example.devmobile_gym.presentation.screens.UserProfessor.adicionaEdi
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.devmobile_gym.data.repository.AulaRepository
+import com.example.devmobile_gym.domain.model.Aula
+import com.example.devmobile_gym.domain.repository.AulaRepositoryModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.util.Date
 
 class AdicionaEditaAulaViewModel() : ViewModel() {
 
-    private val _tipoAula = mutableStateOf("")
-    val tipoAula: State<String> = _tipoAula
 
-    private val _horario = mutableStateOf("")
-    val horario: State<String> = _horario
+    private val aulasRepository : AulaRepositoryModel = AulaRepository()
+    private val _tipoAula = MutableStateFlow("")
+    val tipoAula: StateFlow<String> = _tipoAula.asStateFlow()
+
+    private val _professor = MutableStateFlow("")
+    val professor: StateFlow<String> = _professor.asStateFlow()
+
+    private val _status = MutableStateFlow("")
+    val status: StateFlow<String> = _status.asStateFlow()
+
+    // KAIO LEMBRAR DE TRANSFORMAR ISSO AQUI PARA INT QUANDO FOR POR NO BANCO DE DADOS PELO AMOR DE DEUS
+    private val _alocacaoMax = MutableStateFlow("")
+    val alocacaoMax: StateFlow<String> = _alocacaoMax.asStateFlow()
+
+    // Novo estado: data e hora da aula
+    private val _dataHoraSelecionada = MutableStateFlow<Date?>(null)
+    val dataHoraSelecionada: StateFlow<Date?> = _dataHoraSelecionada.asStateFlow()
+
+    fun atualizarDataHora(data: Date) {
+        _dataHoraSelecionada.value = data
+    }
+
+    fun onProfessorChange(newText: String) {
+        _professor.value = newText
+    }
+
+    fun onAlocacaoMaxChange(newAlocacaoMax: String) {
+        _alocacaoMax.value = newAlocacaoMax
+    }
 
     fun onTipoAulaChange(newSearch: String) {
         _tipoAula.value = newSearch
     }
 
-    fun onHorarioChange(newSearch: String) {
-        _horario.value = newSearch
+    fun isNumeroInteiro(valor: String): Boolean {
+        val regex = Regex("^\\d+$") // aceita apenas dígitos (0-9)
+        return regex.matches(valor)
     }
+
+
+    fun criarAula(aula: Aula?, onSuccess: () -> Unit, onError: () -> Unit) {
+        viewModelScope.launch {
+            val aulaCriada = aula?.let { aulasRepository.createAula(it) }
+
+            if (aulaCriada == true) {
+                _status.value = "Aula criada com sucesso."
+                onSuccess()
+            } else {
+                _status.value = "Erro ao criar a aula."
+                onError()
+            }
+        }
+
+    }
+
 }

afeeef6fd8815762d806b07c646167ac18e082a8
Kaio <kakacnmendes@gmail.com>
2025-05-25 09:23:00 -0300

    refactor: altera o model do aluno para receber as funcionalidades do streak e do historico

--- app/src/main/java/com/example/devmobile_gym/domain/model/Aluno.kt
+++ app/src/main/java/com/example/devmobile_gym/domain/model/Aluno.kt
@@ -1,8 +1,19 @@
 package com.example.devmobile_gym.domain.model
 
 data class Aluno(
-    val uid: String = "",
+    val uid: String = "", // gerado pelo banco de dados
+
+    // informações do cadastro
     val nome: String = "",
     val email: String = "",
+
+    // geradas ao treinar
+    val diasTreinados: List<String> = emptyList(),
+    val historico: List<Treino> = emptyList(),
+    val exerciciosRealizados: List<String> = emptyList(),
+    val ultimoDiaTreinado: String = "",
+    val exercicioFavorito: String = "",
+
+    // gerada pelo professor(a)
     val rotina: List<String> = emptyList()
 )

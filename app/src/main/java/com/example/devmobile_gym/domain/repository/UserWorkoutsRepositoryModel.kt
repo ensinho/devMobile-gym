e7b397bcd96d630aab847ca9fc9d15fa32efaaab
Kaio <kakacnmendes@gmail.com>
2025-05-26 14:44:20 -0300

    feat: adiciona as funcoes para  atualizar e criar o streak

--- app/src/main/java/com/example/devmobile_gym/domain/repository/UserWorkoutsRepositoryModel.kt
+++ app/src/main/java/com/example/devmobile_gym/domain/repository/UserWorkoutsRepositoryModel.kt
@@ -1,5 +1,9 @@
 package com.example.devmobile_gym.domain.repository
 
-interface UserWorkoutsModel {
-    suspend fun criarStreak()
+import com.example.devmobile_gym.domain.model.UserWorkouts
+
+interface UserWorkoutsRepositoryModel {
+    suspend fun criarStreak(userId: String)
+    suspend fun atualizarStreakDoAluno(userWorkouts: UserWorkouts)
+    suspend fun obterStreakPorUserId(userId: String): UserWorkouts?
 }
